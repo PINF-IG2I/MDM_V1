8ee27d9e56158d0a8880ddc8e74b545763c945ce
Zelouiix2 <31691898+Zelouiix2@users.noreply.github.com>
2018-03-09 08:06:10 +0100

    Add files via upload

--- templates/administration.php
+++ templates/administration.php
@@ -13,64 +13,10 @@ if ($_SESSION["status"]!="Administrateur")
 }
 
 include_once "libs/modele.php";
-
+$languageList=getLanguages();
 $users=listerUtilisateurs();
 
 ?>
-<!DOCTYPE html>
-<!-- saved from url=(0064)http://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/# -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>MDM - Alstom</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-  <!-- Bootstrap core CSS -->
-  <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="./bootstrap/css/sticky-footer-navbar.css" rel="stylesheet">
-
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-
-
-
-
-</head>
-
-<body>
-
-  <header>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="http://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/#">Fixed navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <span class="navbar-nav mr-auto">
-        </span>
-        <form class="form-inline mt-2 mt-md-0">
-          <select id="selectLanguage">
-            <option value="" disabled selected><?php echo $translation["language"]?></option>
-            <?php 
-
-            foreach ($languageList as $key => $value) {
-              echo "<option value='".$value["language"]."'>".$value["language"]."</option>";
-            }
-
-
-            ?>
-          </select>
-        </form>
-      </div>
-    </nav>
-  </header>
-
 
   <div class="container">
 		<!-- USER MANAGEMENT -->
@@ -78,7 +24,8 @@ $users=listerUtilisateurs();
 		<form class="form-inline">
 			<input class="form-control mb-2 mr-sm-2 mb-sm-0 col-lg-4 col-lg-offset-4" type="text" placeholder="<?php echo $translation["user_name"]?>" />
 			<button type="submit" class="btn btn-primary"><?php echo $translation["search"]?></button>
-		</form>
+		</form><br/>
+		<button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></button>
 		<div id="tabUsers" >
 			<center>
 			<table class="table table-hover">
@@ -93,7 +40,7 @@ $users=listerUtilisateurs();
 				<?php
 					foreach ($users as $tabUsers)
 					{
-						echo "<tr>";
+						echo "<tr onclick='editUser()' id='$tabUsers[last_name]'>";
 						echo "<td>$tabUsers[id_user]</td>";
 						echo "<td>$tabUsers[last_name]</td>";
 						echo "<td>$tabUsers[first_name]</td>";
@@ -106,6 +53,66 @@ $users=listerUtilisateurs();
 			</table>
 			</center>
 		</div>
+			<!-- HIDDEN BOX TO EDIT USER -->
+			<div id="editUser" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 25px; display:none; position:absolute; width: 45%; height: 60%; border: 1px solid black; top: 100px; background-color: lightgray; padding: 5px;" >
+				<center><h3><strong><?php echo $translation["user"]?></strong></h3></center>
+				<form class="form-horizontal" action="controleur.php">
+					<div class="form-group">
+						<label for="number" class="col-sm-3 control-label"><?php echo $translation["number"]?></label>
+						<div class="col-xs-4">
+							<input id="number" class="form-control" type="text" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="col-sm-3 control-label"><?php echo $translation["password"]?></label>
+						<div class="col-xs-4">
+							<input id="password" class="form-control" type="password" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="last_name" class="col-sm-3 control-label"><?php echo $translation["last_name"]?></label>
+						<div class="col-sm-4">
+							<input id="last_name" class="form-control" type="text" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="first_name" class="col-sm-3 control-label"><?php echo $translation["first_name"]?></label>
+						<div class="col-sm-4">
+							<input id="first_name" class="form-control" type="text" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="language" class="col-sm-3 control-label"><?php echo $translation["language"]?></label>
+						<div class="col-sm-4">
+							<select id="language" class="form-control">
+								<option value="" disabled selected><?php echo $translation["language"]?></option>
+								<?php 
+									foreach ($languageList as $key => $value) {
+									  echo "<option value='".$value["language"]."'>".$value["language"]."</option>";
+									}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="status" class="col-sm-3 control-label"><?php echo $translation["status"]?></label>
+						<div class="col-sm-4">
+							<select id="status" class="form-control">
+								<option value="" disabled selected><?php echo $translation["status"]?></option>
+								<option value="internal"><?php echo $translation["internal"]?></option>
+								<option value="external"><?php echo $translation["external"]?></option>
+								<option value="inhibited"><?php echo $translation["inhibited"]?></option>
+								<option value="manager"><?php echo $translation["manager"]?></option>
+								<option value="administrator"><?php echo $translation["administrator"]?></option>
+							</select>
+						</div>
+					</div><br/>
+					<center><button type="submit" class="btn btn-success" name="action"><?php echo $translation["edit"]?></button>
+					<button type="submit" class="btn btn-danger" name="action"><?php echo $translation["delete"]?></button></center>
+				</form><br/>
+			</div>
+			<!-- END HIDDEN BOX TO EDIT USER -->
+			
 		<!-- END USER MANAGEMENT -->
 		
 		<!-- DATABASE MANAGEMENT -->
@@ -114,7 +121,7 @@ $users=listerUtilisateurs();
 			<center>
 			<form action="controleur.php" >
 				<button class="btn btn-default" name="action" value="importDB" ><?php echo $translation["importDB"]?> <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></button>
-				<button class="btn btn-default" name="action" value="saveDB" ><?php echo $translation["saveDB"]?> <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></button>
+				<button class="btn btn-success" name="action" value="saveDB" ><?php echo $translation["saveDB"]?> <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></button>
 				<button class="btn btn-danger" name="action" value="resetDB" ><?php echo $translation["resetDB"]?> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
 			</form>
 			</center>
@@ -122,3 +129,9 @@ $users=listerUtilisateurs();
 		<!-- END DATABASE MANAGEMENT -->
 		
   </div>
+  <script>
+	function editUser()
+	{
+		document.getElementById("editUser").style.display="block";
+	}
+  </script>
